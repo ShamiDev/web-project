4163c8ec1c0d8a7f278bfd63df3a5f7c870d77b6
Hisham Hasan <hishhasanstudent@gmail.com>
2024-05-16 03:34:19 +0500

    Made Navbar go to Admin and Faculty. Removed Workouts. Added styling

--- NASCON_MERN/frontend/src/components/Navbar.js
+++ NASCON_MERN/frontend/src/components/Navbar.js
@@ -1,16 +1,23 @@
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from 'react-router-dom';
+import "./components.css";
 
 const Navbar = () => {
-
   return (
-    <header>
+    <header className="navbar">
       <div className="container">
-        <Link to="/">
-          <h1>NASCON Event Manager</h1>
-        </Link>
+        <h1>NASCON Management System</h1>
+        <div className="nav-buttons">
+          <Link to="/Admin">
+            <button className="nav-button">Admin</button>
+          </Link>
+          <Link to="/Faculty">
+            <button className="nav-button">Faculty</button>
+          </Link>
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- NASCON_MERN/frontend/src/pages/Home.js
+++ NASCON_MERN/frontend/src/pages/Home.js
@@ -1,43 +1,42 @@
-import { useEffect } from "react"
-import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
-
-// pages and components
-import WorkoutDetails from "../components/WorkoutDetails"
-import WorkoutForm from "../components/WorkoutForm"
-
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import Navbar from "../components/Navbar"; // Import the Navbar component
+// import WorkoutDetails from "../components/WorkoutDetails";
+// import WorkoutForm from "../components/WorkoutForm";
+// import { Link } from "react-router-dom";
 
 const Home = () => {
-  const { workouts, dispatch } = useWorkoutsContext()
+  const { workouts, dispatch } = useWorkoutsContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts')
-      const json = await response.json()
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
 
       if (response.ok) {
-        dispatch({type: 'SET_WORKOUTS', payload: json})
+        dispatch({ type: "SET_WORKOUTS", payload: json });
       }
-    }
+    };
 
-    fetchWorkouts()
-  }, [dispatch])
+    fetchWorkouts();
+  }, [dispatch]);
 
   return (
     <div className="home">
-      <div className="workouts">
-        {workouts && workouts.map(workout => (
-          <WorkoutDetails workout={workout} key={workout._id} />
-        ))}
+      <Navbar /> {/* Use the Navbar component */}
+      {/* <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails workout={workout} key={workout._id} />
+          ))}
       </div>
-      <WorkoutForm />
+      <WorkoutForm /> */}
 
-      {/* Button to navigate to Admin component */}
-      <Link to="/Admin">
+      {/* <Link to="/Admin">
         <button>Go to Admin</button>
-      </Link>
+      </Link> */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
